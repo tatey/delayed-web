67673e5d913bc940caa6bf078c62613658d50369
Tate Johnson <tate@tatey.com>
2013-06-06 22:41:16 +1000

    Build example job doubles.

--- app/models/delayed/dashboard/job/double.rb
+++ app/models/delayed/dashboard/job/double.rb
@@ -2,9 +2,21 @@ module Delayed
   module Dashboard
     class Job::Double
       attr_accessor :id
+      attr_accessor :created_at
+      attr_accessor :run_at
+      attr_accessor :attempts
+      attr_accessor :locked_at
+      attr_accessor :locked_by
+      attr_accessor :last_error
 
       def initialize attributes = {}
-        @id = attributes[:id]
+        @id         = attributes[:id]
+        @created_at = attributes[:created_at]
+        @run_at     = attributes[:run_at]
+        @attempts   = attributes[:attemps]
+        @locked_at  = attributes[:locked_at]
+        @locked_by  = attributes[:locked_by]
+        @last_error = attributes[:last_error]
       end
 
       def to_param
@@ -12,13 +24,28 @@ module Delayed
       end
 
       def self.find *args
-        new id: 1
+        all.sample
       end
 
       def self.all
-        5.times.map do |id|
-          new id: id
-        end
+        [build_executing, build_failed, build_queued]
+      end
+
+      def self.build_executing
+        new id: 1, created_at: 1.minute.ago, run_at: 1.minute.ago,
+          attemps: 1, last_error: nil, locked_at: 30.seconds.ago,
+          locked_by: 'host.local'
+      end
+
+      def self.build_failed
+        new id: 2, created_at: 5.hours.ago, run_at: 2.hours.from_now,
+          attemps: 4, last_error: 'RuntimeError: RuntimeError',
+          locked_at: nil, locked_by: nil
+      end
+
+      def self.build_queued
+        new id: 3, created_at: 30.seconds.ago, run_at: 30.seconds.ago,
+          attempts: 0, last_error: nil, locked_at: nil, locked_by: nil
       end
     end
   end
