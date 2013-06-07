1627bccd4a50d97f3b85825e7d5899e010815263
Tate Johnson <tate@tatey.com>
2013-06-07 19:47:19 +1000

    Styling and showing handler.

--- app/models/delayed/dashboard/job/double.rb
+++ app/models/delayed/dashboard/job/double.rb
@@ -8,6 +8,7 @@ module Delayed
       attr_accessor :locked_at
       attr_accessor :locked_by
       attr_accessor :last_error
+      attr_accessor :handler
 
       def initialize attributes = {}
         @id         = attributes[:id]
@@ -17,6 +18,7 @@ module Delayed
         @locked_at  = attributes[:locked_at]
         @locked_by  = attributes[:locked_by]
         @last_error = attributes[:last_error]
+        @handler    = attributes[:handler]
       end
 
       def to_param
@@ -34,18 +36,19 @@ module Delayed
       def self.build_executing
         new id: 1, created_at: 1.minute.ago, run_at: 1.minute.ago,
           attempts: 1, last_error: nil, locked_at: 30.seconds.ago,
-          locked_by: 'host.local'
+          locked_by: 'host.local', handler: '---'
       end
 
       def self.build_failed
         new id: 2, created_at: 5.hours.ago, run_at: 2.hours.from_now,
           attempts: 4, last_error: 'RuntimeError: RuntimeError',
-          locked_at: nil, locked_by: nil
+          locked_at: nil, locked_by: nil, handler: '---'
       end
 
       def self.build_queued
         new id: 3, created_at: 30.seconds.ago, run_at: 30.seconds.ago,
-          attempts: 0, last_error: nil, locked_at: nil, locked_by: nil
+          attempts: 0, last_error: nil, locked_at: nil, locked_by: nil,
+          handler: '---'
       end
     end
   end
