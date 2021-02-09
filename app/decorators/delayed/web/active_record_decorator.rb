fa295caab41ff6af3561b7fcf622b2f70c75b442
Chris Seelus <chris@imeos.com>
2021-02-09 17:34:40 +0100

    Replace update_attributes method

--- app/decorators/delayed/web/active_record_decorator.rb
+++ app/decorators/delayed/web/active_record_decorator.rb
@@ -2,7 +2,7 @@ module Delayed
   module Web
     class ActiveRecordDecorator < SimpleDelegator
       def queue! now = Time.current
-        update_attributes! run_at: now, failed_at: nil, last_error: nil
+        update! run_at: now, failed_at: nil, last_error: nil
       end
     end
   end
